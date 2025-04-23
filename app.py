cfc59141dd186b5c3c8c68e055c2e759852289d8
Thomas Barrett <trbarrettjr@users.noreply.github.com>
2025-04-23 15:34:27 -0600

    Update app.py
    Added error handling for bad ip's

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ space = ' '
 
 reader = geoip2.database.Reader('/app/GeoLite2-ASN.mmdb')
 
+bad_ips = []
+
 with open('ip_list.txt', 'r') as file:
   for ip in file:
     response = reader.asn(ip.strip())
-    print(f'IP: {ip.strip()}' + space * (25 - len(f'IP: {ip.strip()}')) + f'ISP: {response.autonomous_system_organization}')
+    if response:
+      print(f'IP: {ip.strip()}' + space * (25 - len(f'IP: {ip.strip()}')) + f'ISP: {response.autonomous_system_organization}')
+    else:
+      bad_ips.append(ip.strip())
+
+  print()
+  print("List of Bad IP's")
+  for ip in bad_ips:
+    print(f"Bad Reporting: {ip}")
